43afa0209a42c1ca5890b2c2c99710950d896de0
VincentCastig <vincent.castig@gmail.com>
2024-11-26 09:26:27 -0600

    download results to csv feature

--- src/hooks/components/planningTables/step-three.js
+++ src/hooks/components/planningTables/step-three.js
@@ -1,30 +1,19 @@
 import React from 'react';
 
-import Beer from '../../../img/beer-bottle2.webp';
-// import WhiteWine from '../../../img/white-wine.png';
-// import RedWine from '../../../img/red-wine.png';
-// // import RoseWine from '../../../img/rose-1.png';
-// import RoseWine from '../../../img/rose.png';
-// import Bourbon from '../../../img/bourbon.png';
-// import Scotch from '../../../img/scotch.png';
-// import Tequila from '../../../img/tequila.png';
+import Beer from '../../../img/party-images/beer-bottle2.webp';
+import WhiteWine from '../../../img/party-images/white-wine-bottle.png';
+import RedWine from '../../../img/party-images/red-wine-bottle.png';
+import RoseWine from '../../../img/party-images/rose.png';
+import Whiskey from '../../../img/party-images/whiskey-bottle-2.png';
+import Scotch from '../../../img/party-images/scotch-bottle.png';
+import Tequila from '../../../img/party-images/tequila-bottle.png';
+import Gin from '../../../img/party-images/gin-bottle.png';
+import Vodka from '../../../img/party-images/vodka-bottle.png';
+import Rum from '../../../img/party-images/rum-bottle.png';
+import Champagne from '../../../img/party-images/champagne-bottle.png';
 
-import WhiteWine from '../../../img/white-wine-bottle.png';
-import RedWine from '../../../img/red-wine-bottle.png';
-import RoseWine from '../../../img/rose.png';
-
-import Whiskey from '../../../img/whiskey-bottle-2.png';
-import Scotch from '../../../img/scotch-bottle.png';
-import Tequila from '../../../img/tequila-bottle.png';
-
-import Gin from '../../../img/gin-bottle.png';
-import Vodka from '../../../img/vodka-bottle.png';
-import Rum from '../../../img/rum-bottle.png';
-import Champagne from '../../../img/champagne-bottle.png';
-
-
-const StepThree = ({ values, drinksTotal, userDefinedPercentages, nextStep, prevStep }) => {
-    const { drinks, drinkInventory, people } = values;
+const StepThree = ({ values, drinksTotal, userDefinedPercentages, prevStep }) => {
+    const { drinkInventory } = values;
 
     const imageMap = {
         Beer,
@@ -40,18 +29,48 @@ const StepThree = ({ values, drinksTotal, userDefinedPercentages, nextStep, prev
         Champagne
     };
 
+    // Function to download the data as CSV
+    const downloadCSV = () => {
+        const rows = [];
+        rows.push(['Alcohol Type', '0.75L Bottle Count', '1L Bottle Count', '12 Oz Beer Count']); // CSV header
+
+        let total075L = 0;
+        let total1L = 0;
+
+        Object.keys(bottlesNeeded).forEach(drink => {
+            if (drink === 'Beer') {
+                const { totalBottles075L } = bottlesNeeded[drink];
+                rows.push([drink, '', '', totalBottles075L]); // CSV data
+            }
+            else {
+                const { totalBottles075L, totalBottles1L } = bottlesNeeded[drink];
+                rows.push([drink, totalBottles075L, totalBottles1L]); // CSV data
+                total075L += totalBottles075L;
+                total1L += totalBottles1L;
+            }
+        });
+        rows.push([]);
+        rows.push(['Total', total075L, total1L]);
+
+        const csvContent = rows.map(e => e.join(",")).join("\n");
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'party_bottle_counts.csv'; // Set file name
+        link.click();
+    };
+
     // Calculate the total number of bottles for each selected alcohol type
     const calculateBottles = () => {
         const bottlesNeeded = {};
 
         // Loop through the drink inventory
         drinkInventory.forEach(drink => {
-            const { label, defaultPercentage, bottleSize, drinksPerBottle } = drink;
+            const { label, drinksPerBottle } = drink;
 
             // Calculate the drink percentage based on the user input or default value
             const drinkPercentage = (userDefinedPercentages[label]) * drinksTotal / 100; // Total drinks based on the user-defined percentage
             const totalBottles = Math.ceil(drinkPercentage / drinksPerBottle); // Calculate total bottles needed
-
             // Calculate for 0.75-liter and 1-liter bottles
             const totalBottles1L = Math.ceil(totalBottles * 0.75);
             const totalBottles075L = Math.ceil(totalBottles); // Adjust for 1-liter
@@ -69,7 +88,7 @@ const StepThree = ({ values, drinksTotal, userDefinedPercentages, nextStep, prev
         <div className="main-card">
             <h2>Bottle Count Results</h2>
             <div className="results-content">
-                {/* <h2>Bottle Requirements</h2> */}
+                {/* <h2>Include count of spirit bottles here</h2> */}
                 <ul className="bottle-list">
                     {Object.keys(bottlesNeeded).map(drink => (
                         <li key={drink} className="bottle-list-item">
@@ -79,7 +98,6 @@ const StepThree = ({ values, drinksTotal, userDefinedPercentages, nextStep, prev
                                 </div>
                                 <div className='step3-total-type'>{drink}:</div>
                                 <div className='step3-total-box'>
-                                    {/* {drink}: */}
                                     <div>
                                         {drink === 'Beer' ? (
                                             // Only show 0.75L for beer
@@ -102,6 +120,9 @@ const StepThree = ({ values, drinksTotal, userDefinedPercentages, nextStep, prev
             <div className="centered button-row">
                 <button type="button" className="planning-btn btn-secondary btn" onClick={prevStep}>Back</button>
                 {/* <button type="button" className="planning-btn btn-primary btn" onClick={nextStep}>Finish</button> */}
+                
+                    <button onClick={downloadCSV} className="planning-btn btn-secondary btn">Download</button>
+                
             </div>
         </div>
     );
